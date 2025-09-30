7b6b737a57ac8e8321776b7a7a64a47b95c1b67f
Jack Burton <jackb898@gmail.com>
2025-09-30 11:19:02 -0400

    Refactor extract-cron.py for improved functionality

--- extract-cron.py
+++ extract-cron.py
@@ -46,7 +46,7 @@ START_VID = 831394104
 CHUNK_SIZE = 1000
 PASS_LIMIT = 3
 GAP_THRESHOLD = 10000
-FORWARD_BUFFER = 3000
+FORWARD_BUFFER = 200
 MAX_RESTARTS = 1
 REQUEST_DELAY = 0.001  # 1 ms
 
@@ -295,6 +295,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
